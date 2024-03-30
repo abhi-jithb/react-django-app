7f0f1a00662d4cc0254e838de884b28295744265
Abhijith B <babhijith58@gmail.com>
2024-03-30 18:53:56 +0000

    installation added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+// import './App.css';
+import { editTodoForm } from './components/editTodoForm';
+import { todoForm } from './components/todoForm';
+import { todoWrapper } from './components/todoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <todoWrapper />
+
+    </div >
+  );
+}
+
+export default App;
---
+++ src/components/todoForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const todoForm = () => {
+    return (
+        <>
+            <h1>hello it's todoform</h1>
+        </>
+    )
+}
